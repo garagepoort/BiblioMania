3aea776a0e5455107242aa48805c096b96ab948e
David Maes <david.s.maes@gmail.com>
2015-06-07 21:53:17 +0200

    Changed sliding panel layout

--- app/views/book/bookSlidingPanel.blade.php
+++ app/views/book/bookSlidingPanel.blade.php
@@ -1,180 +1,188 @@
 <div id="slide" class="book-detail-div">
-		<div id="book-detail-close-div" class="book-detail-close">
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="margin-left: -7px"></span>
-		</div>
-		<div class="book-detail-container">
-			<h1 id="book-detail-title">title</h1>
-			<h4 id="book-detail-subtitle">subtitle</h4>
-			<div class="book-detail-main-info">
-				<img id="book-detail-coverimage" src="" width="142px" height="226px"/>
-				<table>
-					<tr>
-						<div id="star-detail" class="col-md-6"></div>
-					</tr>
-					<tr>
-						<div class="book-details-info-container">
-							<table class="book-detail-main-info-table">
-								<tr>
-									<td><label>Auteur:</label></td>
-									<td><label id='book-detail-author'></label></td>
-								</tr>
-								<tr>
-									<td><label>ISBN:</label></td>
-									<td><label id='book-detail-isbn'></label></td>
-								</tr>
-								<tr>
-									<td><label>Uitgever</label></td>
-									<td><label id='book-detail-publisher'></label></td>
-								</tr>
-								<tr>
-									<td><label>Land</label></td>
-									<td><label id='book-detail-country'></label></td>
-								</tr>
-								<tr>
-									<td><label>Publicatiedatum:</label></td>
-									<td><label id='book-detail-publication-date'></label></td>
-								</tr>
-								<tr>
-									<td><label>Genre:</label></td>
-									<td><label id='book-detail-genre'></label></td>
-								</tr>
-								<tr>
-									<td><label>Boekenreeks:</label></td>
-									<td><label id='book-detail-serie'></label></td>
-								</tr>
-								<tr>
-									<td><label>Uitgeverreeks:</label></td>
-									<td><label id='book-detail-publisher-serie'></label></td>
-								</tr>
-							</table>
-						</div>
-					</tr>
-				</table>
+    <div id="book-detail-close-div" class="book-detail-close">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true" style="margin-left: -7px"></span>
+    </div>
 
-			</div>
+    <div class="book-detail-container">
+        <h1 id="book-detail-title">title</h1>
+        <h4 id="book-detail-subtitle">subtitle</h4>
 
-			<div class="book-detail-summary">
-				<p id="book-detail-summary"></p>
-			</div>
+        <div class="book-detail-main-info">
+            <img id="book-detail-coverimage" src="" width="142px" height="226px"/>
+            <table>
+                <tr>
+                    <div id="star-detail" class="col-md-6"></div>
+                </tr>
+                <tr>
+                    <div class="book-details-info-container">
+                        <table class="book-detail-main-info-table" width="100%">
+                            <tr>
+                                <td><label>Auteur:</label></td>
+                                <td><label id='book-detail-author'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>ISBN:</label></td>
+                                <td><label id='book-detail-isbn'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>Uitgever</label></td>
+                                <td><label id='book-detail-publisher'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>Land</label></td>
+                                <td><label id='book-detail-country'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>Publicatiedatum:</label></td>
+                                <td><label id='book-detail-publication-date'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>Genre:</label></td>
+                                <td><label id='book-detail-genre'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>Boekenreeks:</label></td>
+                                <td><label id='book-detail-serie'></label></td>
+                            </tr>
+                            <tr>
+                                <td><label>Uitgeverreeks:</label></td>
+                                <td><label id='book-detail-publisher-serie'></label></td>
+                            </tr>
+                        </table>
+                    </div>
+                </tr>
+            </table>
 
-			<table class="book-detail-info-table">
-				<tr>
-					<td>
-						<div class="book-detail-small-info-panel">
-							<table>
-								<tr>
-									<td><label>Cover prijs:</label></td>
-									<td><label id='book-detail-retail-price'></label></td>
-								</tr>
-								<tr>
-									<td><label>Pagina's:</label></td>
-									<td><label id='book-detail-number-of-pages'></label></td>
-								</tr>
-								<tr>
-									<td><label>Druk:</label></td>
-									<td><label id='book-detail-print'></label></td>
-								</tr>
-								<tr>
-									<td><label>Vertaler:</label></td>
-									<td><label id='book-translator'></label></td>
-								</tr>
-							</table>
-						</div>
-					</td>
-					<td>
-						<div class="book-detail-small-info-panel">
-							<table>
-								<tr>
-									<td><label>In collectie:</label></td>
-									<td><input onclick="return false" type="checkbox" id='book-detail-owned'></td>
-								</tr>
-								<tr>
-									<td><label>Gelezen:</label></td>
-									<td><input onclick="return false" type="checkbox" id='book-detail-read'/></td>
-								</tr>
-								<tr class="buy-info-tr">
-									<td><label>Aanschafdatum:</label></td>
-									<td><label id='book-detail-buy-info-date'></label></td>
-								</tr>
-								<tr class="buy-info-tr">
-									<td><label>Aankoopprijs:</label></td>
-									<td><label id='book-detail-buy-info-price-payed'></label></td>
-								</tr>
-								<tr class="buy-info-tr">
-									<td><label>Winkel:</label></td>
-									<td><label id='book-detail-buy-info-shop'></label></td>
-								</tr>
-								<tr class="buy-info-tr">
-									<td><label>Stad winkel:</label></td>
-									<td><label id='book-detail-buy-info-city'></label></td>
-								</tr>
-								<tr class="buy-info-tr">
-									<td><label>Reden:</label></td>
-									<td><label id='book-detail-buy-info-reason'></label></td>
-								</tr>
-								<tr class="buy-info-tr">
-									<td><label>Land gekocht:</label></td>
-									<td><label id='book-detail-buy-info-country'></label></td>
-								</tr>
-								<tr class="gift-info-tr">
-									<td><label>Ontvangstdatum:</label></td>
-									<td><label id='book-detail-gift-info-date'></label></td>
-								</tr>
-								<tr class="gift-info-tr">
-									<td><label>Gekregen van:</label></td>
-									<td><label id='book-detail-gift-info-from'></label></td>
-								</tr>
-								<tr class="gift-info-tr">
-									<td><label>Gelegenheid:</label></td>
-									<td><label id='book-detail-gift-info-occasion'></label></td>
-								</tr>
-								<tr class="gift-info-tr">
-									<td><label>Reden:</label></td>
-									<td><label id='book-detail-gift-info-reason'></label></td>
-								</tr>
-							</table>
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td colspan="2">
-						<div class="book-detail-small-info-panel">
-							<legend>Eerste druk</legend>
-							<table>
-								<tr>
-									<td><label>Titel:</label></td>
-									<td><label id='book-detail-first-print-title'></label></td>
-								</tr>
-								<tr>
-									<td><label>Ondertitel:</label></td>
-									<td><label id='book-detail-first-print-subtitle'></label></td>
-								</tr>
-								<tr>
-									<td><label>ISBN:</label></td>
-									<td><label id='book-detail-first-print-isbn'></label></td>
-								</tr>
-								<tr>
-									<td><label>Land:</label></td>
-									<td><label id='book-detail-first-print-country'></label></td>
-								</tr>
-								<tr>
-									<td><label>Taal:</label></td>
-									<td><label id='book-detail-first-print-language'></label></td>
-								</tr>
-								<tr>
-									<td><label>Uitgever:</label></td>
-									<td><label id='book-detail-first-print-publisher'></label></td>
-								</tr>
-								<tr>
-									<td><label>Publicatie datum:</label></td>
-									<td><label id='book-detail-first-print-publication-date'></label></td>
-								</tr>
-							</table>
-						</div>
-					</td>
-				</tr>
-			</table>
+        </div>
 
-		</div>
+        <div class="card-row">
+            <div class="book-detail-summary material-card">
+                <div class="material-card-title">Samenvatting</div>
+                <div class="material-card-content">
+                    <p id="book-detail-summary"></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-row">
+            <div class="book-detail-small-info-panel material-card card-column-left">
+                <div class="material-card-title">Eerste druk</div>
+                <div class="material-card-content">
+                    <table width="100%">
+                        <tr>
+                            <td><label>Cover prijs:</label></td>
+                            <td><label id='book-detail-retail-price'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Pagina's:</label></td>
+                            <td><label id='book-detail-number-of-pages'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Druk:</label></td>
+                            <td><label id='book-detail-print'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Vertaler:</label></td>
+                            <td><label id='book-translator'></label></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+
+            <div class="book-detail-small-info-panel material-card card-column-right">
+                <div class="material-card-title">Info</div>
+                <div class="material-card-content">
+                    <table width="100%">
+                        <tr>
+                            <td><label>In collectie:</label></td>
+                            <td><input onclick="return false" type="checkbox" id='book-detail-owned'></td>
+                        </tr>
+                        <tr>
+                            <td><label>Gelezen:</label></td>
+                            <td><input onclick="return false" type="checkbox" id='book-detail-read'/></td>
+                        </tr>
+                        <tr class="buy-info-tr">
+                            <td><label>Aanschafdatum:</label></td>
+                            <td><label id='book-detail-buy-info-date'></label></td>
+                        </tr>
+                        <tr class="buy-info-tr">
+                            <td><label>Aankoopprijs:</label></td>
+                            <td><label id='book-detail-buy-info-price-payed'></label></td>
+                        </tr>
+                        <tr class="buy-info-tr">
+                            <td><label>Winkel:</label></td>
+                            <td><label id='book-detail-buy-info-shop'></label></td>
+                        </tr>
+                        <tr class="buy-info-tr">
+                            <td><label>Stad winkel:</label></td>
+                            <td><label id='book-detail-buy-info-city'></label></td>
+                        </tr>
+                        <tr class="buy-info-tr">
+                            <td><label>Reden:</label></td>
+                            <td><label id='book-detail-buy-info-reason'></label></td>
+                        </tr>
+                        <tr class="buy-info-tr">
+                            <td><label>Land gekocht:</label></td>
+                            <td><label id='book-detail-buy-info-country'></label></td>
+                        </tr>
+                        <tr class="gift-info-tr">
+                            <td><label>Ontvangstdatum:</label></td>
+                            <td><label id='book-detail-gift-info-date'></label></td>
+                        </tr>
+                        <tr class="gift-info-tr">
+                            <td><label>Gekregen van:</label></td>
+                            <td><label id='book-detail-gift-info-from'></label></td>
+                        </tr>
+                        <tr class="gift-info-tr">
+                            <td><label>Gelegenheid:</label></td>
+                            <td><label id='book-detail-gift-info-occasion'></label></td>
+                        </tr>
+                        <tr class="gift-info-tr">
+                            <td><label>Reden:</label></td>
+                            <td><label id='book-detail-gift-info-reason'></label></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="card-row">
+            <div class="book-detail-small-info-panel material-card">
+                <div class="material-card-title">Eerste druk</div>
+                <div class="material-card-content">
+                    <table width="100%">
+                        <tr>
+                            <td><label>Titel:</label></td>
+                            <td><label id='book-detail-first-print-title'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Ondertitel:</label></td>
+                            <td><label id='book-detail-first-print-subtitle'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>ISBN:</label></td>
+                            <td><label id='book-detail-first-print-isbn'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Land:</label></td>
+                            <td><label id='book-detail-first-print-country'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Taal:</label></td>
+                            <td><label id='book-detail-first-print-language'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Uitgever:</label></td>
+                            <td><label id='book-detail-first-print-publisher'></label></td>
+                        </tr>
+                        <tr>
+                            <td><label>Publicatie datum:</label></td>
+                            <td><label id='book-detail-first-print-publication-date'></label></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
