438f3419115070bb55cb60878a4732a27bdac9e2
David Maes <david.s.maes@gmail.com>
2015-06-28 20:53:35 +0200

    Change google api

--- app/config/production/googleAPI.php
+++ app/config/production/googleAPI.php
@@ -3,5 +3,5 @@
 return array(
 	'client_id' => '61777699766-bcvsasp62krl4rnb5tv40c8t25etkmja.apps.googleusercontent.com',
 	'client_secret' => 'B06d-Ftuaq6u_eY7FR8X_jbo',
-	'redirect_uri' => 'http://www.bendani-cooperation.com/Bibliomania/googleAuthentication',
+	'redirect_uri' => 'http://www.bendani-cooperation.com/BiblioMania/googleAuthentication',
 );
