f84968b5b25b352f7c940a368a0bf279e2eda73a
David Maes <david.s.maes@gmail.com>
2015-06-29 22:40:09 +0200

    fixed authorlist validation

--- public/assets/js/author/authorsList.js
+++ public/assets/js/author/authorsList.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#name, #firstname, #infix").editable({
+    $("#name, #firstname").editable({
         validate: function(value) {
             if($.trim(value) == '') {
                 return 'This field is required';
@@ -19,8 +19,9 @@ $(document).ready(function() {
     });
     $('#date_of_birth, #date_of_death').editable({
         validate: function(value) {
-            var regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-            if(!regex.test(value)) {
+            // regular expression to match required date format
+            var re = /^\d{1,2}\-\d{1,2}\-\d{4}$/;
+            if(value != '' && !value.match(re)) {
                 return 'Formaat dd-mm-yyyy';
             }
         }
