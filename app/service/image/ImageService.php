6ae283471affc749ca688233eaadcc5cc32fdfa1
David Maes <david.s.maes@gmail.com>
2015-07-01 22:39:41 +0200

    Fix image scaling

--- app/service/image/ImageService.php
+++ app/service/image/ImageService.php
@@ -5,15 +5,19 @@ class ImageService
 
     public function saveUploadImageForAuthor($image, $filename){
         $filename = str_random(8) . '_' . $filename . '.jpg';
-        $filename = StringUtils::clean($filename);
-
+        $location = Config::get("properties.authorImagesLocation") .  "/" . $filename;
         $image->move(Config::get("properties.authorImagesLocation"), $filename);
+        $image = imagecreatefromjpeg($location);
+        $this->resizeAndSaveImage($location, $image);
         return $filename;
     }
 
     public function saveUploadImageForBook($image, $filename){
         $filename = str_random(8) . '_' . $filename . '.jpg';
-        $image->move(Config::get("properties.bookImagesLocation"). "/" . Auth::user()->username, $filename);
+        $location = Config::get("properties.bookImagesLocation") . "/" . Auth::user()->username . "/" . $filename;
+        $image->move(Config::get("properties.bookImagesLocation") . "/" . Auth::user()->username, $filename);
+        $image = imagecreatefromjpeg($location);
+        $this->resizeAndSaveImage($location, $image);
         return $filename;
     }
 
@@ -31,24 +35,27 @@ class ImageService
     {
         $img = file_get_contents($url);
         $im = imagecreatefromstring($img);
-        $width = imagesx($im);
-        $height = imagesy($im);
         $newwidth = '142';
         $newheight = '226';
 
-        $thumb = imagecreatetruecolor($newwidth, $newheight);
-
-        imagecopyresized($thumb, $im, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-
+        $newImage = $this->resize_image_max($im, $newwidth, $newheight);
         $imageFilename = str_random(8) . '_' . $imageLocation . '.jpg';
         $imageFilename = StringUtils::clean($imageFilename);
         $imageLocation = $folder . '/' . $imageFilename;
-        imagejpeg($thumb, $imageLocation); //save image as jpg
+        imagejpeg($newImage, $imageLocation); //save image as jpg
 
-        imagedestroy($thumb);
-
+        imagedestroy($newImage);
         imagedestroy($im);
         return $imageFilename;
+    }
+
+    function resizeAndSaveImage($location, $image){
+        $newwidth = '142';
+        $newheight = '226';
+        $newImage = $this->resize_image_max($image, $newwidth, $newheight);
+        imagejpeg($newImage, $location); //save image as jpg
+        imagedestroy($image);
+        imagedestroy($newImage);
     }
 
     public function removeAuthorImage($image)
@@ -69,6 +76,30 @@ class ImageService
                 unlink($fullImagePath);
             }
         }
+    }
+
+    function resize_image_max($image,$max_width,$max_height) {
+        $w = imagesx($image); //current width
+        $h = imagesy($image); //current height
+        if ((!$w) || (!$h)) { $GLOBALS['errors'][] = 'Image couldn\'t be resized because it wasn\'t a valid image.'; return false; }
+
+        if (($w <= $max_width) && ($h <= $max_height)) { return $image; } //no resizing needed
+
+        //try max width first...
+        $ratio = $max_width / $w;
+        $new_w = $max_width;
+        $new_h = $h * $ratio;
+
+        //if that didn't work
+        if ($new_h > $max_height) {
+            $ratio = $max_height / $h;
+            $new_h = $max_height;
+            $new_w = $w * $ratio;
+        }
+
+        $new_image = imagecreatetruecolor ($new_w, $new_h);
+        imagecopyresampled($new_image,$image, 0, 0, 0, 0, $new_w, $new_h, $w, $h);
+        return $new_image;
     }
 
 }
