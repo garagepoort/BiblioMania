86205585eaba35f03dc9a9a60b3de30efe85b006
garagepoort <david.s.maes@gmail.com>
2015-07-29 19:10:30 +0200

    Added files for desktop

---
+++ app/config/local_linux_mint/properties.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+	'backupScriptLocation' => '~/Development/HuurApplicatie/backups/',
+	'mysqlLocation' => '/Applications/MAMP/Library/bin/',
+	'bookImagesLocation' => 'bookImages',
+	'authorImagesLocation' => 'authorImages',
+	'questionImage' => 'questionCover.png',
+);
