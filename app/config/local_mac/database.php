9b3a43370046e1a1927bb54b9a59d0518baa13e1
David Maes <davidmaes@ACAs-MacBook-Pro-2.local>
2015-08-04 16:07:56 +0200

    added configurations for mac

---
+++ app/config/local_mac/database.php
@@ -0,0 +1,50 @@
+<?php
+
+return array(
+
+	
+	'connections' => array(
+
+		'sqlite' => array(
+			'driver'   => 'sqlite',
+			'database' => __DIR__.'/../database/production.sqlite',
+			'prefix'   => '',
+		),
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'bibliomania',
+			'username'  => 'root',
+			'password'  => 'root',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'unix_socket'   => '/var/run/mysqld/mysqld.sock',
+			'prefix'    => '',
+			'port'    => '3306',
+		),
+
+		'pgsql' => array(
+			'driver'   => 'pgsql',
+			'host'     => 'localhost',
+			'database' => 'database',
+			'username' => 'root',
+			'password' => '',
+			'charset'  => 'utf8',
+			'prefix'   => '',
+			'schema'   => 'public',
+		),
+
+		'sqlsrv' => array(
+			'driver'   => 'sqlsrv',
+			'host'     => 'localhost',
+			'database' => 'database',
+			'username' => 'root',
+			'password' => '',
+			'prefix'   => '',
+		),
+
+	),
+
+
+);
