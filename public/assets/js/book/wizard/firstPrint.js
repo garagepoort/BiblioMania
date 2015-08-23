8328f9e21acb41cf77eafb510fb5a9c1c1ac54c4
David Maes <david.s.maes@gmail.com>
2015-08-23 11:44:31 +0200

    Do form validation when switching wizardsteps through stepsBar

--- public/assets/js/book/wizard/firstPrint.js
+++ public/assets/js/book/wizard/firstPrint.js
@@ -2,4 +2,33 @@ $(document).ready(function () {
     $('#first_print_country').autocomplete({
         lookup: window.country_names
     });
+
+    // VALIDATORS
+    formValidator = $('.createBookForm').bootstrapValidator({
+        message: 'This value is not valid',
+        message: 'This value is not valid',
+        ignore: '',
+        framework: 'bootstrap',
+        excluded: [':disabled'],
+        feedbackIcons: {
+            valid: '',
+            invalid: '',
+            validating: ''
+        },
+        fields: {
+            //FIRST PRINT
+            first_print_isbn: {
+                validators: {
+                    regexp: {
+                        regexp: '^[0-9]{13}$',
+                        message: 'Exact 13 cijfers'
+                    }
+                }
+            }
+        }
+    }).on('error.field.bv', function (e, data) {
+        data.bv.disableSubmitButtons(false);
+    }).on('success.field.bv', function (e, data) {
+        data.bv.disableSubmitButtons(false);
+    });
 });
