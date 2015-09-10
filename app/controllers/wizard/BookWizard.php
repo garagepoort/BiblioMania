5c62f805e89f67a09bc426449c9d86e2f935dc1a
David Maes <david.s.maes@gmail.com>
2015-09-10 20:43:14 +0200

    implementing wizard module

--- app/controllers/wizard/BookWizard.php
+++ app/controllers/wizard/BookWizard.php
@@ -1,5 +1,7 @@
 <?php
 
+use Bendani\PhpCommon\WizardService\Model\Wizard;
+
 class BookWizard extends Wizard
 {
 
--- app/controllers/wizard/FirstPrintWizardStep.php
+++ app/controllers/wizard/FirstPrintWizardStep.php
@@ -1,5 +1,7 @@
 <?php
 
+use Bendani\PhpCommon\WizardService\Model\WizardStep;
+
 class FirstPrintWizardStep extends WizardStep
 {
     private $bookFolder = "book/wizard/";
