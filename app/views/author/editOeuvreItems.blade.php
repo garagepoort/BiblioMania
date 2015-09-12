d0e78b4057c0214a014272e23f7b850f505f8a5e
David Maes <david.s.maes@gmail.com>
2015-09-12 02:27:23 +0200

    split adding and editing of oeuvre items

---
+++ app/views/author/editOeuvreItems.blade.php
@@ -0,0 +1,83 @@
+<div id='edit-oeuvre-textarea-panel' hidden="true">
+    <textarea placeholder="<id> - <jaar> - <titel>" id='edit-oeuvre-textarea' name="oeuvre" cols="80"rows="5" style="width: 100%"></textarea>
+    <button class='btn btn-default' id='oeuvreButton' onclick="editOeuvreItems(); return false;">Pas oeuvre aan</button>
+</div>
+
+<script type="text/javascript">
+    var oeuvre_json = {{ $oeuvre_json }};
+    $(function () {
+        hideEditOevreItems();
+        fillInOeuvreTextArea();
+    });
+
+    function showEditOevreItems(){
+        $('#edit-oeuvre-textarea-panel').show();
+    }
+    function hideEditOevreItems(){
+        $('#edit-oeuvre-textarea-panel').hide();
+    }
+
+    function toggleEditOevreItems(){
+        $('#edit-oeuvre-textarea-panel').toggle();
+    }
+
+
+    function fillInOeuvreTextArea(){
+        var oeuvreString = "";
+        $.each(oeuvre_json, function (index, obj) {
+            oeuvreString = oeuvreString + obj.id + " - " + obj.publication_year + " - " + obj.title + "\n";
+        });
+        $('#edit-oeuvre-textarea').val(oeuvreString);
+    }
+
+
+    function editOeuvreItems() {
+        var errorMessage = validateOeuvreList();
+        if (errorMessage) {
+            showError(errorMessage);
+        } else {
+            hideError();
+            editOeuvre();
+        }
+    }
+
+    function editOeuvre() {
+        $.post(baseUrl + "/editBookFromAuthors",
+                {
+                    author_id: '{{ $author_id }}',
+                    oeuvre: $('#edit-oeuvre-textarea').val()
+                },
+                function (data, status) {
+                    if (status === "success") {
+                        location.reload();
+                    }
+                }).fail(function (data) {
+                    BootstrapDialog.show({
+                        message: data.responseJSON.message
+                    });
+                });
+    }
+
+
+    function validateOeuvreList() {
+        var res = $('#edit-oeuvre-textarea').val().split("\n");
+        var errorMessage = null;
+        if (res != null && res != "") {
+            $.each(res, function (index, obj) {
+                var splitString = obj.split(" - ");
+                if (splitString.length < 3) {
+                    errorMessage = "Formaat is id - jaar - titel";
+                } else {
+                    var id = splitString[0];
+                    var year = splitString[1];
+                    var title = splitString[2];
+                    if (!title) {
+                        errorMessage = "Titel moet ingevuld zijn";
+                    }
+                }
+            });
+        }
+
+        return errorMessage;
+    }
+</script>
