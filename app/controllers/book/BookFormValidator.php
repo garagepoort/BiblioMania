b2d4f6e8ad070520c1a0fd76258dd5e6f5fead9f
David Maes <david.s.maes@gmail.com>
2015-09-15 19:34:54 +0200

    removed all old routes and views

--- app/controllers/book/BookFormValidator.php
+++ app/controllers/book/BookFormValidator.php
@@ -2,25 +2,6 @@
 
 class BookFormValidator {
 
-    public function createValidator(){
-        $rules = array(
-            'book_title' => 'required',
-            'book_author' => 'required',
-            'book_isbn' => 'required|numeric|digits:13',
-            'book_number_of_pages' => 'numeric',
-            'book_print' => 'numeric',
-            'book_publisher' => 'required',
-            'book_genre' => 'required',
-            'author_name' => 'required',
-            'author_firstname' => 'required',
-            'first_print_isbn' => 'numeric|digits:13',
-            'first_print_publication_date' => 'date_format:"d/m/Y"'
-        );
-        $validator = Validator::make(Input::get(), $rules);
-
-        return $validator;
-    }
-
     public function createValidatorForBasics(){
         $rules = array(
             'book_title' => 'required',
