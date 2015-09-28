054cc78fed8791adfa9bad30ab5caf18c96bff3b
David Maes <david.s.maes@gmail.com>
2015-09-28 11:27:30 +0200

    coverted library information and filter into bookmark sliding panels

--- app/views/library/libraryInformationSlidingPanel.blade.php
+++ app/views/library/libraryInformationSlidingPanel.blade.php
@@ -1,3 +1,36 @@
-<div id="libraryInformationLslidingPanel">
-
+<div id="libraryInformationSlidingPanel" class="sliding-panel libraryInformationSlidingPanel">
+    <div id="libraryInformationSlidingPanelTitle" class="slidingPanelTitle material-card-title">Informatie collectie</div>
+    <div class="material-card-content">
+        <div class="row">
+            {{ Form::label('amountOfBooksLabel', 'Aantal boeken:' , array('class' => 'control-label col-md-9')); }}
+            {{ Form::label('amountOfBooks', $total_amount_of_books , array('class' => 'control-label col-md-2')); }}
+        </div>
+        <div class="row">
+            {{ Form::label('valueLabel', 'Aantal boeken in bezit:' , array('class' => 'control-label col-md-9')); }}
+            {{ Form::label('value', $total_amount_of_books_owned, array('class' => 'control-label col-md-2')); }}
+        </div>
+        <div class="row">
+            {{ Form::label('valueLabel', 'Waarde bibliotheek:' , array('class' => 'control-label col-md-9')); }}
+            {{ Form::label('value', $total_value_library  . ' euro', array('class' => 'control-label col-md-2')); }}
+        </div>
+    </div>
+    <div id="libraryInformationBookMark" class="bookMark libraryInformationBookmark">
+        {{ HTML::image('images/info_icon.png', 'information',array('class'=>'info-icon')) }}
+    </div>
 </div>
+
+<script type="text/javascript">
+    $(function () {
+        var slidingPanel = new BorderSlidingPanel($('#libraryInformationSlidingPanel'), "left");
+        $('#libraryInformationSlidingPanel').on('mouseover', function () {
+            slidingPanel.open(function () {
+
+            });
+        });
+        $('#libraryInformationSlidingPanel').on('mouseout', function () {
+            slidingPanel.close(function () {
+
+            });
+        });
+    });
+</script>
