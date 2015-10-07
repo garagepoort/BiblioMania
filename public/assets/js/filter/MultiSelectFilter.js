a95038a4931b985589bad141b8411cbb81a03ba2
David Maes <david.s.maes@gmail.com>
2015-10-07 23:31:50 +0200

    made javascript objects for filtering

---
+++ public/assets/js/filter/MultiSelectFilter.js
@@ -0,0 +1,51 @@
+function MultiSelectFilter(filterId, field, operators, options){
+    this.filterId = filterId;
+    this.field = field;
+    this.options = options;
+    this.operators = operators;
+    this.inputElement;
+}
+
+MultiSelectFilter.prototype.createFilterElement = function(){
+    var formgroup = $("<div class=\"form-group\"></div>");
+    formgroup.attr("forFilter", this.filterId);
+
+    var inputgroup = $("<div class=\"col-md-10 filter-input\"></div>");
+
+    var input = $("<select class=\"filterInput input-sm form-control\" multiple=\"multiple\"></select>");
+    input.attr("filterOperator", this.operators[Object.keys(this.operators)[0]]);
+    input.attr("filterInputId", this.filterId);
+
+    for (var option in this.options) {
+        var optionEl = $('<option>' + option + '</option>');
+        optionEl.attr('value', this.options[option]);
+        input.append(optionEl);
+    }
+
+
+    if (Object.keys(this.operators).length > 1) {
+        var operatorSelect = new OperatorSelector(this.filterId, this.operators, input);
+        input.addClass("operator-input")
+        inputgroup.append(operatorSelect.createElement());
+        this.operatorSelector = operatorSelect;
+    }
+    inputgroup.append(input);
+
+
+    formgroup.append("<label class='control-label col-md-10'>" + this.field + "</label>");
+    formgroup.append(inputgroup);
+    input.multiselect();
+    this.inputElement = input;
+    return formgroup;
+}
+
+MultiSelectFilter.prototype.getValue = function(){
+    return this.inputElement.val();
+}
+
+MultiSelectFilter.prototype.getSelectedOperator = function(){
+    if(this.operatorSelector == undefined){
+        return this.operators[Object.keys(this.operators)[0]];
+    }
+    return this.operatorSelector.getSelectedOperator();
+}
