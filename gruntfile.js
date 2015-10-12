9df29afc02bfaa564af7ae58108ac57e5e0ea132
David Maes <david.s.maes@gmail.com>
2015-10-12 18:19:29 +0200

    use grunt to minify js and css files

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        concat: {
+            js: {
+                src: ['app/assets/main/**/*.js'],
+                dest: 'app/assets/concatMain.js'
+            }
+        },
+        uglify: {
+            js: {
+                src: 'app/assets/concatMain.js',
+                dest: 'public/assets/main.min.js'
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'public/assets/main.min.css': ['app/assets/main/**/*.css']
+                }
+            }
+        }
+    });
+
+// load plugins
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+// register at least this one task
+    grunt.registerTask('default', [ 'concat', 'uglify', 'cssmin' ]);
+
+
+};
