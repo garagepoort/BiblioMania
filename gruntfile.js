8ce5b497fe553e5e8c73bf946cedf433fba9a5b9
David Maes <david.s.maes@gmail.com>
2015-10-12 19:34:27 +0200

    remove concatenated js file after minify

--- gruntfile.js
+++ gruntfile.js
@@ -23,7 +23,8 @@ module.exports = function (grunt) {
                     'public/assets/main.min.css': ['app/assets/main/**/*.css']
                 }
             }
-        }
+        },
+        clean: ['app/assets/concatMain.js']
     });
 
 // load plugins
@@ -31,9 +32,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
 // register at least this one task
-    grunt.registerTask('default', [ 'concat', 'uglify', 'cssmin' ]);
+    grunt.registerTask('default', [ 'concat', 'uglify', 'cssmin', 'clean']);
 
 
 };
