4e263f1f079edf1efedcd38ebd753628ce34c3a5
David Maes <david.s.maes@gmail.com>
2015-10-14 22:24:30 +0200

    added and used resourceUtlities.

--- public/assets/js/author/BookFromAuthorService.js
+++ public/assets/js/author/BookFromAuthorService.js
@@ -10,24 +10,40 @@ function BookFromAuthorService() {
  * */
 BookFromAuthorService.deleteBookFromAuthor = function (options) {
     var DELETE_BOOK_FROM_AUTHOR_URL = baseUrl + "/deleteBookFromAuthor";
-    $.post(DELETE_BOOK_FROM_AUTHOR_URL,
-        {bookFromAuthorId: options.bookFromAuthorId},
-        function (data, status) {
-            if (status === "success") {
-                if(options.onSuccess){
-                    options.onSuccess();
-                }
-                if (options.showNotifications) {
-                    showNotification('', 'Succesvol verwijdert.', 'success');
-                }
-            }
-        })
-        .fail(function (data) {
-            if (options.onFailure) {
-                options.onFailure();
-            }
-            if (options.showNotifications) {
-                showNotification('Opgelet!', data.responseJSON.message, 'danger');
-            }
-        });
+
+    var postOptions = {
+        url: DELETE_BOOK_FROM_AUTHOR_URL,
+        data: {bookFromAuthorId: options.bookFromAuthorId},
+        onSuccess: options.onSuccess,
+        onFailure: options.onFailure
+    }
+    if(options.showNotifications){
+        postOptions.onSuccessNotification = 'Succesvol verwijdert.';
+    }
+
+    ResourceUtilities.doPost(postOptions);
+}
+
+/*
+ * options
+ *      - bookFromAuthorId
+ *      - bookId
+ *      - onSuccess [optional]
+ *      - onFailure [optional]
+ *      - showNotifications [optional, default: false]
+ * */
+BookFromAuthorService.linkBook = function (options) {
+    var LINK_BOOK_FROM_AUTHOR_URL = baseUrl + "/linkBookToBookFromAuthor";
+
+    var postOptions = {
+        url: LINK_BOOK_FROM_AUTHOR_URL,
+        data: { book_from_author_id: options.bookFromAuthorId, book_id: options.bookId},
+        onSuccess: options.onSuccess,
+        onFailure: options.onFailure
+    }
+    if(options.showNotifications){
+        postOptions.onSuccessNotification = 'Succesvol gelinkt aan boek.';
+    }
+
+    ResourceUtilities.doPost(postOptions);
 }
