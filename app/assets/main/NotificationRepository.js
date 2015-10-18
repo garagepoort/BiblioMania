bbba813618650fc756fc66a3021d3133c8c07b9a
David Maes <david.s.maes@gmail.com>
2015-10-18 21:36:20 +0200

    BIB-109: added notification repository. Added notification when book is saved

--- app/assets/main/NotificationRepository.js
+++ app/assets/main/NotificationRepository.js
@@ -1,5 +1,17 @@
 function NotificationRepository(){}
 
+NotificationRepository.getNotifications = function() {
+    if(!sessionStorage.getItem("notifications")){
+        var notifications = [];
+        NotificationRepository.setNotifications(notifications);
+    }
+    return JSON.parse(sessionStorage.getItem("notifications"));
+}
+
+NotificationRepository.setNotifications = function(notifications) {
+    sessionStorage.setItem("notifications", JSON.stringify(notifications));
+}
+
 /*
 * notification
 *       - title
@@ -7,21 +19,27 @@ function NotificationRepository(){}
 *       - type
 * */
 NotificationRepository.addNotification = function(notification){
-    var notifications = sessionStorage.getItem("notifications");
-    if(!notifications){
-        notifications = [];
-    }
+    var notifications = NotificationRepository.getNotifications();
     notifications.push(notification);
-    sessionStorage.setItem("notifications", notifications);
+    NotificationRepository.setNotifications(notifications);
 };
 
 NotificationRepository.showNotifications = function(){
-    var notifications = sessionStorage.getItem("notifications");
+    var notifications = NotificationRepository.getNotifications();
     if(!notifications){
         notifications = [];
     }
     for (var i = 0; i < notifications.length; i++) {
         var notification = notifications[i];
         showNotification(notification.title, notification.message, notification.type);
+        notification.remove = true;
     }
+    notifications = notifications.filter(function (notification) {
+        if (notification.remove) {
+            return false;
+        }
+        return true;
+    });
+
+    NotificationRepository.setNotifications(notifications);
 };
