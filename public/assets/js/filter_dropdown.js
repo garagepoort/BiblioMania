a0bd1e979c1146850f8a22bc80cdb9b17d1f62b5
David Maes <david.s.maes@gmail.com>
2015-10-19 21:39:10 +0200

    Change order by select box to a dropdown. Layout changes

--- public/assets/js/filter_dropdown.js
+++ public/assets/js/filter_dropdown.js
@@ -6,9 +6,7 @@ $(document).ready(function(e){
         $('.search-panel-operator span#search_concept').text(concept);
         $('#search_param_operator').val(param);
     });
-});
 
-$(document).ready(function(e){
     $('.search-panel-type .dropdown-menu').find('a').click(function(e) {
         e.preventDefault();
         var param = $(this).attr("href").replace("#","");
