131b2e72aee158764ce088acbdbcbd10dccce278
David Maes <david.s.maes@gmail.com>
2015-11-09 23:18:54 +0100

    BIB-117:  pass model to wizard template

--- app/assets/main/angular/book/wizard/com.bendani.bibliomania.book.wizard.controller.js
+++ app/assets/main/angular/book/wizard/com.bendani.bibliomania.book.wizard.controller.js
@@ -1,11 +1,18 @@
 'use strict';
 
-angular.module('com.bendani.bibliomania.book.wizard.controller', ['com.bendani.bibliomania.error.container', 'com.bendani.bibliomania.book.wizard.model'])
-    .controller('BookWizardController', ['$scope', 'BookWizard','ErrorContainer',function ($scope, BookWizard, ErrorContainer) {
+angular.module('com.bendani.bibliomania.book.wizard.controller', ['com.bendani.bibliomania.error.container', 'com.bendani.bibliomania.book.wizard.model', 'com.bendani.bibliomania.book.model'])
+    .controller('BookWizardController', ['$scope', 'Book', 'BookWizard','ErrorContainer', '$routeParams',function ($scope, Book, BookWizard, ErrorContainer, $routeParams) {
 
         function init(){
+            $scope.book = {};
             $scope.$parent.title = 'Boek wijzigen';
             retrieveBookWizard();
+
+            var bookId = $routeParams.bookId;
+
+            if(bookId !== undefined){
+                retrieveFullBook(bookId);
+            }
         }
 
         function retrieveBookWizard(){
@@ -13,5 +20,10 @@ angular.module('com.bendani.bibliomania.book.wizard.controller', ['com.bendani.b
                 $scope.steps = steps;
             }, ErrorContainer.handleRestError);
         }
+
+        function retrieveFullBook(newValue) {
+            $scope.book = Book.get({id: newValue}, function (data) {}, ErrorContainer.handleRestError);
+        }
+
         init();
     }]);
