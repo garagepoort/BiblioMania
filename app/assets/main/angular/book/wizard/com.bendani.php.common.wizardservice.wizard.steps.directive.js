eaef3fcdc326e3d56f4f9bc2bf4030ba85cf2233
David Maes <david.s.maes@gmail.com>
2015-11-09 22:55:14 +0100

    BIB-117:  added first changes to create the book wizard with angular

---
+++ app/assets/main/angular/book/wizard/com.bendani.php.common.wizardservice.wizard.steps.directive.js
@@ -0,0 +1,27 @@
+angular
+    .module('com.bendani.php.common.wizardservice.wizard.steps.directive', [])
+    .directive('wizardSteps', function (){
+        return {
+            scope: {
+                steps: "=",
+                progressStep: "@",
+                currentStep: "@"
+            },
+            restrict: "E",
+            templateUrl: "../BiblioMania/views/partials/book/wizard/wizard-steps-directive.html",
+            controller: ['$scope', function($scope) {
+                $scope.getWizardClass = function(step){
+                    if(step.number == $scope.currentStep){
+                        return 'current';
+                    }
+                    if($scope.progressStep == 'COMPLETE' || step.number < $scope.progressStep){
+                        return 'before';
+                    }
+                    if(step.number == $scope.progressStep){
+                        return 'progress';
+                    }
+                    return '';
+                }
+            }]
+        };
+    });
--- app/controllers/book/BookStepController.php
+++ app/controllers/book/BookStepController.php
@@ -7,4 +7,14 @@ class BookStepController extends StepController
     public function getWizard(){
         return new BookWizard();
     }
+
+    public function getBookWizard(){
+        $steps = array_map(function($item, $index){
+            return array(
+                "title"=>$item->getTitle(),
+                "number"=>$index
+            );
+        }, $this->getWizard()->getAllSteps(), array_keys($this->getWizard()->getAllSteps()));
+        return $steps;
+    }
 }
