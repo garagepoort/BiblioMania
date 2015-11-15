873bc50ae0d9cd3e6bf47866f47a38df4204749a
David Maes <david.s.maes@gmail.com>
2015-11-15 22:12:38 +0100

    create book in wizard

--- app/service/wizard/BookBasicsService.php
+++ app/service/wizard/BookBasicsService.php
@@ -26,34 +26,50 @@ class BookBasicsService
         $this->genreService = App::make('GenreService');
     }
 
+    public function createBookBasics(BaseBookBasicsRequest $baseBookBasicsRequest){
+        /** @var Book $book */
+        $book = new Book();
+        $book->user_id = Auth::user()->id;
+        $this->saveBook($baseBookBasicsRequest, $book);
+    }
 
     public function updateBookBasics(UpdateBookBasicsRequest $updateBookBasicsRequest){
         /** @var Book $book */
         $book = $this->bookRepository->find($updateBookBasicsRequest->getId());
-        $genre = $this->genreService->getGenreByName($updateBookBasicsRequest->getGenre());
+        $this->saveBook($updateBookBasicsRequest, $book);
+    }
+
+    /**
+     * @param BaseBookBasicsRequest $baseBookBasicsRequest
+     * @param $book
+     * @throws ServiceException
+     */
+    private function saveBook(BaseBookBasicsRequest $baseBookBasicsRequest, $book)
+    {
+        $genre = $this->genreService->getGenreByName($baseBookBasicsRequest->getGenre());
 
         Ensure::objectNotNull("Book", $book);
         Ensure::objectNotNull("Genre", $genre);
-        Ensure::objectNotNull("PublicationDate", $updateBookBasicsRequest->getPublicationDate());
-        Ensure::stringNotBlank("Language", $updateBookBasicsRequest->getLanguage());
-        Ensure::stringNotBlank("Title", $updateBookBasicsRequest->getTitle());
-        Ensure::stringNotBlank("Isbn", $updateBookBasicsRequest->getIsbn());
-        Ensure::stringNotBlank("Publisher", $updateBookBasicsRequest->getPublisher());
-        Ensure::stringNotBlank("Country", $updateBookBasicsRequest->getCountry());
+        Ensure::objectNotNull("PublicationDate", $baseBookBasicsRequest->getPublicationDate());
+        Ensure::stringNotBlank("Language", $baseBookBasicsRequest->getLanguage());
+        Ensure::stringNotBlank("Title", $baseBookBasicsRequest->getTitle());
+        Ensure::stringNotBlank("Isbn", $baseBookBasicsRequest->getIsbn());
+        Ensure::stringNotBlank("Publisher", $baseBookBasicsRequest->getPublisher());
+        Ensure::stringNotBlank("Country", $baseBookBasicsRequest->getCountry());
 
-        $bookPublisher = $this->publisherService->findOrCreate($updateBookBasicsRequest->getPublisher());
-        $country = $this->countryService->findOrCreate($updateBookBasicsRequest->getCountry());
-        $book->language()->associate($this->languageService->findOrSave($updateBookBasicsRequest->getLanguage()));
+        $bookPublisher = $this->publisherService->findOrCreate($baseBookBasicsRequest->getPublisher());
+        $country = $this->countryService->findOrCreate($baseBookBasicsRequest->getCountry());
+        $book->language()->associate($this->languageService->findOrSave($baseBookBasicsRequest->getLanguage()));
 
-        $tagsAsStrings = $this->mapTags($updateBookBasicsRequest);
+        $tagsAsStrings = $this->mapTags($baseBookBasicsRequest);
         $tags = $this->tagService->createTags($tagsAsStrings);
 
-        $date = $this->createPublicationDate($updateBookBasicsRequest);
+        $date = $this->createPublicationDate($baseBookBasicsRequest);
         $book->publication_date()->associate($date);
 
-        $book->title = $updateBookBasicsRequest->getTitle();
-        $book->subtitle = $updateBookBasicsRequest->getSubtitle();
-        $book->ISBN = $updateBookBasicsRequest->getIsbn();
+        $book->title = $baseBookBasicsRequest->getTitle();
+        $book->subtitle = $baseBookBasicsRequest->getSubtitle();
+        $book->ISBN = $baseBookBasicsRequest->getIsbn();
         $book->genre_id = $genre->id;
         $book->publisher_id = $bookPublisher->id;
         $book->publisher_country_id = $country->id;
@@ -63,27 +79,30 @@ class BookBasicsService
     }
 
     /**
-     * @param UpdateBookBasicsRequest $updateBookBasicsRequest
+     * @param BaseBookBasicsRequest $baseBookBasicsRequest
      * @return array
      */
-    private function mapTags(UpdateBookBasicsRequest $updateBookBasicsRequest)
+    private function mapTags(BaseBookBasicsRequest $baseBookBasicsRequest)
     {
+        if($baseBookBasicsRequest->getTags() == null){
+            return array();
+        }
         $tagsAsStrings = array_map(function ($item) {
             return $item->getText();
-        }, $updateBookBasicsRequest->getTags());
+        }, $baseBookBasicsRequest->getTags());
         return $tagsAsStrings;
     }
 
     /**
-     * @param UpdateBookBasicsRequest $updateBookBasicsRequest
+     * @param BaseBookBasicsRequest $baseBookBasicsRequest
      * @return Date
      */
-    private function createPublicationDate(UpdateBookBasicsRequest $updateBookBasicsRequest)
+    private function createPublicationDate(BaseBookBasicsRequest $baseBookBasicsRequest)
     {
         $date = new Date();
-        $date->day = $updateBookBasicsRequest->getPublicationDate()->getDay();
-        $date->month = $updateBookBasicsRequest->getPublicationDate()->getDay();
-        $date->year = $updateBookBasicsRequest->getPublicationDate()->getYear();
+        $date->day = $baseBookBasicsRequest->getPublicationDate()->getDay();
+        $date->month = $baseBookBasicsRequest->getPublicationDate()->getDay();
+        $date->year = $baseBookBasicsRequest->getPublicationDate()->getYear();
         $date->save();
         return $date;
     }
