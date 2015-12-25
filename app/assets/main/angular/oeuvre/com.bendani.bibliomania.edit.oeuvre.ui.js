315073959125dea5089f3c060e730a1cc8660c00
David Maes <david.s.maes@gmail.com>
2015-12-25 13:34:15 +0100

    When saving n oeuvre go back to previous page

--- app/assets/main/angular/oeuvre/com.bendani.bibliomania.edit.oeuvre.ui.js
+++ app/assets/main/angular/oeuvre/com.bendani.bibliomania.edit.oeuvre.ui.js
@@ -6,47 +6,54 @@ angular.module('com.bendani.bibliomania.edit.oeuvre.ui',
         'com.bendani.bibliomania.error.container',
         'com.bendani.bibliomania.book.selection.modal.service',
         'com.bendani.bibliomania.confirmation.modal.service',
+        'com.bendani.bibliomania.title.panel',
         'angular-growl',
         'com.bendani.bibliomania.image.fallback'])
-    .config(['$routeProvider',function ($routeProvider) {
+    .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/edit-oeuvre-item/:id', {
             templateUrl: '../BiblioMania/views/partials/oeuvre/edit-oeuvre-item.html',
             controller: 'EditOeuvreItemController'
         });
     }])
-    .controller('EditOeuvreItemController', ['$scope', 'Oeuvre', 'Author', 'ErrorContainer', 'growl', '$routeParams', 'BookSelectionModalService', 'ConfirmationModalService', function ($scope, Oeuvre, Author, ErrorContainer, growl, $routeParams, BookSelectionModalService, ConfirmationModalService) {
+    .controller('EditOeuvreItemController', ['$scope', 'Oeuvre', 'Author', 'ErrorContainer', 'growl', '$routeParams', 'BookSelectionModalService', 'ConfirmationModalService', 'TitlePanelService',
+        function ($scope, Oeuvre, Author, ErrorContainer, growl, $routeParams, BookSelectionModalService, ConfirmationModalService, TitlePanelService) {
 
-        $scope.$parent.title = "Oeuvre";
+            TitlePanelService.setTitle("Oeuvre");
 
-        $scope.books = Oeuvre.books({id: $routeParams.id }, function(){}, ErrorContainer.handleRestError);
-        $scope.model = Oeuvre.get({ id: $routeParams.id }, function(response){
-            $scope.$parent.title = response.title;
-            $scope.author = Author.get({id: response.authorId}, function(){}, ErrorContainer.handleRestError);
-        }, ErrorContainer.handleRestError);
+            $scope.books = Oeuvre.books({id: $routeParams.id}, function () {}, ErrorContainer.handleRestError);
 
-        $scope.submitForm = function(){
-            Oeuvre.update($scope.model, function(){
-                growl.addSuccessMessage("Oeuvre aangepast");
+            $scope.model = Oeuvre.get({id: $routeParams.id}, function (response) {
+                $scope.$parent.title = response.title;
+                $scope.author = Author.get({id: response.authorId}, function () {
+                }, ErrorContainer.handleRestError);
             }, ErrorContainer.handleRestError);
-        };
 
-        $scope.showSelectBookDialog = function () {
-            BookSelectionModalService.show([
-                {id: "book-author", value: [{value: $scope.model.authorId}]}
-            ], function(book){
-                Oeuvre.linkBook({id: $routeParams.id}, {bookId: book.id}, function () {
-                    $scope.books = Oeuvre.books({id: $routeParams.id }, function(){}, ErrorContainer.handleRestError);
-                    growl.addSuccessMessage("Boek gelinked");
+            $scope.submitForm = function () {
+                Oeuvre.update($scope.model, function () {
+                    growl.addSuccessMessage("Oeuvre aangepast");
+                    TitlePanelService.goToPreviousUrl();
                 }, ErrorContainer.handleRestError);
-            });
-        };
+            };
 
-        $scope.deleteBookFromOeuvreItem = function(book){
-            ConfirmationModalService.show('Wilt u de link met het boek "' + book.title + '" verwijderen?', function(){
-                Oeuvre.unlinkBook({id: $routeParams.id}, {bookId: book.id}, function () {
-                    $scope.books = Oeuvre.books({id: $routeParams.id }, function(){}, ErrorContainer.handleRestError);
-                    growl.addSuccessMessage("Boek link verwijderd");
-                }, ErrorContainer.handleRestError);
-            });
-        };
-    }]);
+            $scope.showSelectBookDialog = function () {
+                BookSelectionModalService.show([
+                    {id: "book-author", value: [{value: $scope.model.authorId}]}
+                ], function (book) {
+                    Oeuvre.linkBook({id: $routeParams.id}, {bookId: book.id}, function () {
+                        $scope.books = Oeuvre.books({id: $routeParams.id}, function () {
+                        }, ErrorContainer.handleRestError);
+                        growl.addSuccessMessage("Boek gelinked");
+                    }, ErrorContainer.handleRestError);
+                });
+            };
+
+            $scope.deleteBookFromOeuvreItem = function (book) {
+                ConfirmationModalService.show('Wilt u de link met het boek "' + book.title + '" verwijderen?', function () {
+                    Oeuvre.unlinkBook({id: $routeParams.id}, {bookId: book.id}, function () {
+                        $scope.books = Oeuvre.books({id: $routeParams.id}, function () {
+                        }, ErrorContainer.handleRestError);
+                        growl.addSuccessMessage("Boek link verwijderd");
+                    }, ErrorContainer.handleRestError);
+                });
+            };
+        }]);
