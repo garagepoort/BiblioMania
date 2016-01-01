535a43842e6506a68cc86067a2b28df726fec99a
David Maes <david.s.maes@gmail.com>
2016-01-01 21:07:12 +0100

    BIB-117: added serie overview screen. and edit serie modal.

--- app/assets/main/angular/serie/com.bendani.bibliomania.serie.model.js
+++ app/assets/main/angular/serie/com.bendani.bibliomania.serie.model.js
@@ -1,4 +1,6 @@
 angular.module('com.bendani.bibliomania.serie.model', [ 'ngResource' ])
     .factory('Serie', ['$resource', function ($resource) {
-        return $resource('../BiblioMania/series', {});
+        return $resource('../BiblioMania/series', {}, {
+            update: { method : 'PUT' }
+        });
     } ]);
