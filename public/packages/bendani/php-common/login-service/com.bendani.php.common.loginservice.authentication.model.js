f10401b85d9caebb09c09b32d14b0347de7a098d
David Maes <david.s.maes@gmail.com>
2016-01-02 01:01:39 +0100

    remove files that are generated

--- public/packages/bendani/php-common/login-service/com.bendani.php.common.loginservice.authentication.model.js
+++
@@ -1,5 +0,0 @@
-angular.module('com.bendani.php.common.loginservice.authentication.model', [ 'ngResource' ])
-    .factory('Authentication', ['$rootScope', '$resource', function ($rootScope, $resource) {
-        var url = $rootScope.baseUrl + "login";
-        return $resource(url, {});
-    } ]);
