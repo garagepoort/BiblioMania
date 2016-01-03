a1c6fa389f74553b37d98a00f3b9b3ecd40d7189
David Maes <david.s.maes@gmail.com>
2016-01-03 16:01:27 +0100

    No field on first print info are required.

---
+++ app/database/migrations/2016_01_03_145355_drop_isbn_unique_key_on_first_print.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropIsbnUniqueKeyOnFirstPrint extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('first_print_info', function($table)
+		{
+			$table->dropUnique('first_print_info_isbn_unique');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
