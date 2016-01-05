283fa240c298d5d8b0e10e4aaf2a536269d97651
David Maes <david.s.maes@gmail.com>
2016-01-05 21:41:32 +0100

    restore admincontroller

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -1,12 +1,30 @@
 <?php
+class AdminController extends BaseController{
+    /** @var  AdminService */
+    private $adminService;
+    function __construct()
+    {
+        $this->adminService = App::make('AdminService');
+    }
+    public function goToAdminPagina(){
+        return View::make('admin/admin')->with(array('title'=>'Admin'));
+    }
+    public function backupDatabase($username = null, $password = null){
+        $authenticationSuccessfull = $this->authenticate($username, $password);
+        if($authenticationSuccessfull){
+            $this->adminService->backupDatabase();
+            return Redirect::to('getBooks');
+        }
+        return Redirect::to('login');
+    }
+    private function authenticate($username = null, $password = null){
+        $authenticationSuccessfull = false;
 
-/**
- * Created by PhpStorm.
- * User: davidmaes
- * Date: 05/01/16
- * Time: 21:31
- */
-class AdminController
-{
-
+        if(Auth::check() and Auth::user()->admin == true){
+            $authenticationSuccessfull = true;
+        }else if(Auth::attempt(array('username' => $username, 'password' => $password, 'admin' => true))){
+            $authenticationSuccessfull = true;
+        }
+        return $authenticationSuccessfull;
+    }
 }
