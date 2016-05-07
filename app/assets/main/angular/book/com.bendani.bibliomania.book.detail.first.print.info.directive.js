f9bb2f256416114d904bb11f3473ec35419e812a
David Maes <david.s.maes@gmail.com>
2016-05-07 09:15:16 +0200

    Added karma test for book detail first print info directive

--- app/assets/main/angular/book/com.bendani.bibliomania.book.detail.first.print.info.directive.js
+++ app/assets/main/angular/book/com.bendani.bibliomania.book.detail.first.print.info.directive.js
@@ -1,6 +1,5 @@
 angular
     .module('com.bendani.bibliomania.book.detail.first.print.info.directive', [
-        'com.bendani.bibliomania.book.model',
         'com.bendani.bibliomania.first.print.info.model',
         'com.bendani.bibliomania.first.print.selection.modal.service'
     ])
@@ -11,29 +10,38 @@ angular
             },
             restrict: "E",
             templateUrl: "../BiblioMania/views/partials/book/book-detail-first-print-info-directive.html",
-            controller: ['$scope', '$location','FirstPrintInfo', 'FirstPrintSelectionModalService', 'ConfirmationModalService', 'DateService', 'Book', 'growl', 'ErrorContainer',
-                function ($scope, $location, FirstPrintInfo, FirstPrintSelectionModalService, ConfirmationModalService, DateService, Book, growl, ErrorContainer) {
-
-                    $scope.showSelectFirstPrintDialog = function () {
-                        FirstPrintSelectionModalService.show(function (firstPrint) {
-                            FirstPrintInfo.linkBook({id: firstPrint.id}, {bookId: $scope.book.id}, function(){
-                                $scope.book.firstPrintInfo = FirstPrintInfo.get({id: firstPrint.id}, function(){}, ErrorContainer.handleRestError);
-                                growl.addSuccessMessage('Eerste druk gewijzigd');
-                            }, ErrorContainer.handleRestError);
-                        });
-                    };
-
-                    $scope.createFirstPrintInfo = function(){
-                        $location.path('/create-first-print-and-link-to-book/'+ $scope.book.id);
-                    };
-
-                    $scope.editFirstPrintInfo = function(){
-                        $location.path('/edit-first-print/'+ $scope.book.firstPrintInfo.id);
-                    };
-
-                    $scope.convertDate = function(date){
-                        return DateService.dateToString(date);
-                    };
-                }]
+            controller: ['$location','FirstPrintInfo', 'FirstPrintSelectionModalService', 'DateService', 'growl', 'ErrorContainer', BookDetailFirstPrintInfoController],
+            controllerAs: 'vm',
+            bindToController: true
         };
     });
+
+function BookDetailFirstPrintInfoController($location, FirstPrintInfo, FirstPrintSelectionModalService, DateService, growl, ErrorContainer) {
+    var vm = this;
+
+    vm.showSelectFirstPrintDialog = showSelectFirstPrintDialog;
+    vm.convertDate = convertDate;
+    vm.createFirstPrintInfo = createFirstPrintInfo;
+    vm.editFirstPrintInfo = editFirstPrintInfo;
+
+    function showSelectFirstPrintDialog() {
+        FirstPrintSelectionModalService.show(function (firstPrint) {
+            FirstPrintInfo.linkBook({id: firstPrint.id}, {bookId: vm.book.id}, function(){
+                vm.book.firstPrintInfo = FirstPrintInfo.get({id: firstPrint.id}, function(){}, ErrorContainer.handleRestError);
+                growl.addSuccessMessage('Eerste druk gewijzigd');
+            }, ErrorContainer.handleRestError);
+        });
+    }
+
+    function createFirstPrintInfo(){
+        $location.path('/create-first-print-and-link-to-book/'+ vm.book.id);
+    }
+
+    function editFirstPrintInfo(){
+        $location.path('/edit-first-print/'+ vm.book.firstPrintInfo.id);
+    }
+
+    function convertDate(date){
+        return DateService.dateToString(date);
+    }
+}
---
+++ app/tests/js/book/com.bendani.bibliomania.book.detail.first.print.info.directive_spec.js
@@ -0,0 +1,132 @@
+describe('com.bendani.bibliomania.book.detail.first.print.info.directive', function () {
+
+    describe('BookDetailFirstPrintInfoController', function () {
+
+        var BOOK = {
+            id: 123, firstPrintInfo: {
+                id: 321
+            }
+        };
+
+        var NEW_FIRST_PRINT = {id: 987};
+
+        var vm, html, $httpBackend, $compile, $scope, errorContainerMock, dateServiceMock;
+        var growlMock, locationMock;
+
+        var firstPrintSelectionModalServiceMock = {
+            show: function (callback) {
+                callback(NEW_FIRST_PRINT);
+            }
+        };
+
+        beforeEach(function () {
+            errorContainerMock = jasmine.createSpyObj('errorContainerMock', ['handleRestError', 'setErrorCode']);
+            locationMock = jasmine.createSpyObj('locationMock', ['path']);
+            growlMock = jasmine.createSpyObj('growlMock', ['addSuccessMessage']);
+            dateServiceMock = jasmine.createSpyObj('dateServiceMock', ['dateToString']);
+            spyOn(firstPrintSelectionModalServiceMock, ['show']).and.callThrough();
+
+            html = '<book-detail-first-print-info book="book"></book-detail-first-print-info>';
+
+            module('com.bendani.bibliomania.book.detail.first.print.info.directive', function ($provide) {
+                $provide.value('$location', locationMock);
+                $provide.value('ErrorContainer', errorContainerMock);
+                $provide.value('DateService', dateServiceMock);
+                $provide.value('growl', growlMock);
+                $provide.value('FirstPrintSelectionModalService', firstPrintSelectionModalServiceMock);
+            });
+
+            inject(['$rootScope', '$httpBackend', '$compile', function (_$rootScope_, _$httpBackend_, _$compile_) {
+                $scope = _$rootScope_.$new();
+                $httpBackend = _$httpBackend_;
+                $compile = _$compile_;
+            }]);
+
+        });
+
+        function _createController() {
+            $httpBackend.expectGET('../BiblioMania/views/partials/book/book-detail-first-print-info-directive.html').respond(200, 'somehtml');
+            var directive = $compile(html)($scope);
+            $scope.$digest();
+            $httpBackend.flush();
+            vm = directive.controller('bookDetailFirstPrintInfo');
+
+            vm.book = BOOK;
+        }
+
+        describe('showSelectFirstPrintDialog', function () {
+            it('opensDialog and links book on success', function () {
+                _createController();
+
+                $httpBackend.expectPOST('../BiblioMania/firstprints/' + NEW_FIRST_PRINT.id + '/books', {bookId: BOOK.id}).respond(204);
+                $httpBackend.expectGET('../BiblioMania/firstprints/' + NEW_FIRST_PRINT.id).respond(200, NEW_FIRST_PRINT);
+
+                vm.showSelectFirstPrintDialog();
+
+                $httpBackend.flush();
+
+                expect(vm.book.firstPrintInfo).toEqual(jasmine.objectContaining(NEW_FIRST_PRINT));
+                expect(growlMock.addSuccessMessage).toHaveBeenCalledWith('Eerste druk gewijzigd');
+            });
+
+            it('handles error when linking fails', function () {
+                _createController();
+
+                $httpBackend.expectPOST('../BiblioMania/firstprints/' + NEW_FIRST_PRINT.id + '/books', {bookId: BOOK.id}).respond(500);
+
+                vm.showSelectFirstPrintDialog();
+
+                $httpBackend.flush();
+
+                expect(vm.book.firstPrintInfo).toEqual(jasmine.objectContaining(BOOK.firstPrintInfo));
+                expect(errorContainerMock.handleRestError).toHaveBeenCalled();
+            });
+
+            it('handles error when retrieving new FirstPrintInfo fails', function () {
+                _createController();
+
+                $httpBackend.expectPOST('../BiblioMania/firstprints/' + NEW_FIRST_PRINT.id + '/books', {bookId: BOOK.id}).respond(204);
+                $httpBackend.expectGET('../BiblioMania/firstprints/' + NEW_FIRST_PRINT.id).respond(500);
+
+                vm.showSelectFirstPrintDialog();
+
+                $httpBackend.flush();
+
+                expect(vm.book.firstPrintInfo).toEqual(jasmine.objectContaining(BOOK.firstPrintInfo));
+                expect(errorContainerMock.handleRestError).toHaveBeenCalled();
+            });
+        });
+
+        describe('createFirstPrintInfo', function () {
+            it('redirects to create first print info', function () {
+                _createController();
+
+                vm.createFirstPrintInfo();
+
+                expect(locationMock.path).toHaveBeenCalledWith('/create-first-print-and-link-to-book/' + BOOK.id);
+            });
+        });
+
+        describe('editFirstPrintInfo', function () {
+            it('redirects to edit first print info', function () {
+                _createController();
+
+                vm.editFirstPrintInfo();
+
+                expect(locationMock.path).toHaveBeenCalledWith('/edit-first-print/' + BOOK.firstPrintInfo.id);
+            });
+        });
+
+        describe('convertDate', function () {
+            it('calls convertDate on DateService', function () {
+                var date = {day: 3, month: 3, year: 2016};
+                _createController();
+
+                vm.convertDate(date);
+
+                expect(dateServiceMock.dateToString).toHaveBeenCalledWith(date);
+            });
+        });
+
+    });
+});
