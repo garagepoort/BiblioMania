ae5199fa4b866cbc30345f8335af6578d8b42d88
David Maes <david.maes@lampiris.be>
2017-05-07 23:43:20 +0200

    change layout

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function (grunt) {
     grunt.initConfig({
 
+        less: {
+            development: {
+                files: {
+                    'resources/assets/main/css/custom.css': 'resources/assets/main/css/custom.less',
+                    'resources/assets/main/css/statistics.css': 'resources/assets/main/css/statistics.less'
+                }
+            }
+        },
+
         concat: {
             js: {
                 src: ['resources/assets/main/angular/angular-charts/Chart.min.js','resources/assets/main/angular/angular-charts/angular-chart.min.js', 'resources/assets/main/**/*.js'],
@@ -30,12 +39,13 @@ module.exports = function (grunt) {
 // load plugins
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
 
 // register at least this one task
-    grunt.registerTask('default', [ 'concat', 'uglify', 'cssmin', 'clean']);
+    grunt.registerTask('default', [ 'less','concat', 'uglify', 'cssmin', 'clean']);
 
 
 };
--- resources/views/index.php
+++ resources/views/index.php
@@ -13,6 +13,9 @@
         <script type="text/javascript" src="packages/bendani/php-common/filter-service/filters.min.js"></script>
         <script type="text/javascript" src="packages/bendani/php-common/login-service/loginservice.min.js"></script>
 
+        <script type="text/javascript" src="assets/lib/metisMenu.min.js"></script>
+        <script type="text/javascript" src="assets/lib/metisMenu.min.css"></script>
+
 
         <link href="packages/bendani/php-common/filter-service/filters.min.css" rel="stylesheet"/>
         <link href="packages/bendani/php-common/uiframework/uiframework.min.css" rel="stylesheet"/>
@@ -36,33 +39,71 @@
     </head>
     <body ng-controller="MainController" scrolling-directive>
         <div popover-close exclude-class="exclude">
+
+
             <div growl></div>
 
-            <nav class="navbar navbar-default customNavbar navbar-fixed-top" role="navigation">
-                    <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <a class="navbar-brand" href="#/books">Bibliomania</a>
+            <nav class="navbar navbar-toggleable-md navbar-default navbar-fixed-top" style="margin-bottom: 0px;">
+                <a class="navbar-brand" href="#/books">Bibliomania</a>
+                <div ng-if="loggedInUser" style="float: right; margin-right: 20px; margin-top: 10px;">
+                    <button ng-click="logOut()" id="logout-button" class="btn btn-warning btn-sm">Log out</button>
                 </div>
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" ng-controller="HeaderController">
-                    <ul class="nav navbar-nav">
-                        <li ng-class="{ active: isActive('/books')}"><a href="#/books">Boeken</a></li>
-                        <li ng-class="{ active: isActive('/authors')}"><a href="#/authors">Auteurs</a></li>
-                        <li ng-class="{ active: isActive('/publishers')}"><a href="#/publishers">Uitgevers</a></li>
-                        <li ng-class="{ active: isActive('/series')}"><a href="#/series">Boeken reeksen</a></li>
-                        <li ng-class="{ active: isActive('/publisherseries')}"><a href="#/publisherseries">Uitgever reeksen</a></li>
-                        <li ng-class="{ active: isActive('/statistics')}"><a href="#/statistics">Statistieken</a></li>
-                    </ul>
-                    <div ng-if="loggedInUser" style="float: right; margin-right: 20px; margin-top: 10px;">
-                        <button ng-click="logOut()" id="logout-button" class="btn btn-warning btn-sm">Log out</button>
-                    </div>
-                </div>
-
             </nav>
 
-            <div class="contentContainer">
-                <title-panel></title-panel>
-                <div class="contentPanel" ng-view>
-                    <!--use ng-view to insert content-->
+            <div class="row">
+                <!-- uncomment code for absolute positioning tweek see top comment in css -->
+                <!-- <div class="absolute-wrapper"> </div> -->
+                <!-- Menu -->
+                <div ng-if="loggedInUser" class="side-menu" style="text-align: left; margin-top: 51px;">
+
+                    <nav class="navbar navbar-default" role="navigation">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <div class="brand-wrapper">
+                                <!-- Hamburger -->
+                                <button type="button" class="navbar-toggle">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+
+                                <!-- Brand -->
+<!--                                <div class="brand-name-wrapper">-->
+<!--                                    <a class="navbar-brand" href="#">-->
+<!--                                        Bibliomania-->
+<!--                                    </a>-->
+<!--                                </div>-->
+                            </div>
+
+                        </div>
+
+                        <!-- Main Menu -->
+                        <div class="side-menu-container">
+                            <ul class="nav navbar-nav">
+
+                                <li ng-class="{ active: isActive('/books')}"><a href="#/books"><i class="fa fa-book"></i> Boeken</a></li>
+                                <li ng-class="{ active: isActive('/authors')}"><a href="#/authors"><i class="fa fa-user"></i> Auteurs</a></li>
+                                <li ng-class="{ active: isActive('/publishers')}"><a href="#/publishers"><i class="fa fa-book"></i> Uitgevers</a></li>
+                                <li ng-class="{ active: isActive('/series')}"><a href="#/series"><i class="fa fa-list"></i> Boeken reeksen</a></li>
+                                <li ng-class="{ active: isActive('/publisherseries')}"><a href="#/publisherseries"><i class="fa fa-list"></i> Uitgever reeksen</a></li>
+                                <li ng-class="{ active: isActive('/statistics')}"><a href="#/statistics"><i class="fa fa-line-chart"></i> Statistieken</a></li>
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </nav>
+
+                </div>
+
+                <!-- Main Content -->
+                <div class="container-fluid">
+                    <div class="side-body">
+                        <div class="contentContainer">
+                            <title-panel></title-panel>
+                            <div class="contentPanel" ng-view>
+                                <!--use ng-view to insert content-->
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
