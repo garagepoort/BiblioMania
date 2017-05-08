79ed5ce9200e41593852f53eed41ce4cd36aebf3
David Maes <david.maes@lampiris.be>
2017-05-08 22:39:15 +0200

    fixing e2e tests

--- tests/e2e/datasetbuilders/datasets/UserCanLinkAndUnlinkOeuvreItemToBookDataSet.php
+++ tests/e2e/datasetbuilders/datasets/UserCanLinkAndUnlinkOeuvreItemToBookDataSet.php
@@ -43,7 +43,7 @@ class UserCanLinkAndUnlinkOeuvreItemToBookDataSet implements DataSet
     }
 
     function createUser(){
-        $user = $this->userService->createUser('testUser', 'test@test.be', 'test');
+        $user = $this->userService->createUser(CreateUserRequestBuilder::buildDefault());
         return $user->id;
     }
 
